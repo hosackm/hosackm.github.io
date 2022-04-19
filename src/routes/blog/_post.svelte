dde62866ccac0323ba66a7a152b6934cf9988a48
Matt Hosack <hosack.matt@gmail.com>
2022-04-19 22:27:58 +0200

    Adding tailwind typography for markdown styling, adding prism syntax highlighting styles

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -1,8 +1,24 @@
 <!-- _post.svelte is a layout for our markdown posts -->
 <script lang="ts">
+  import "./styles/prism-one-dark.css";
+
   export let title;
   export let date;
+  export let author;
 </script>
 
-<h1>{title}</h1>
-<p>Published: {date}</p>
+<style lang="postcss">
+  h1 {
+    @apply text-2xl text-gray-900 font-bold;
+  }
+</style>
+
+<section class="my-10">
+  <h1>{title}</h1>
+  <p>Published: {date}</p>
+  <p>By: {author}</p>
+</section>
+
+<article class="prose prose-slate lg:prose-xl">
+  <slot/>
+</article>
